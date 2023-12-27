3f7585eef778bbbac11ba2d52d4bc6b78372bffd
swikars1 <swikarsharma@gmail.com>
2023-12-27 22:08:18 +0545

    playing around with derive affix state and decorators

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
 import { Elysia } from "elysia";
+const setup = new Elysia({ name: "setup" })
+  .decorate({
+    argon: "a",
+    boron: "b",
+    carbon: "c",
+  })
+  .state({
+    token: "87asdbj",
+    id: "1",
+    email: "asd@sd.d",
+  });
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app1 = new Elysia()
+  .use(setup.suffix("decorator", "setup").prefix("state", "authUser"))
+  .derive(({ store }) => {
+    return {
+      bearerToken: "Bearer " + store.authUserToken,
+    };
+  })
+
+  .get("/", ({ carbonSetup, store, bearerToken }) => ({
+    carbonSetup,
+    authUserId: store.authUserId,
+    bearerToken,
+  }));
+
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .get("/swikar", () => ({ swikar: "done" }))
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
